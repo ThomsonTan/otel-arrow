19859402a6dafe8e48c787e28b644cf33070e791
Tom Tan <totan@microsoft.com>
2025-07-01 21:10:01 +0000

    Implement traits for passing data

--- rust/experimental/query_engine/engine-api/src/lib.rs
+++ rust/experimental/query_engine/engine-api/src/lib.rs
@@ -1,11 +1,39 @@
 use data_engine_recordset::{
-    data_expressions::*, logical_expressions::*, primitives::*, value_expressions::*, *,
+    data::*, data_expressions::*, logical_expressions::*, primitives::*, value_expressions::*, *,
 };
 use opentelemetry_proto::tonic::collector::logs::v1::ExportLogsServiceRequest;
 use prost::Message;
-use opentelemetry_proto::tonic::logs::v1::ScopeLogs;
+use opentelemetry_proto::tonic::logs::v1::{ScopeLogs, LogRecord};
 
 use std::sync::Mutex;
+use once_cell::sync::Lazy;
+
+#[derive(Debug, Clone)]
+struct EngineLogRecordBatch(pub ScopeLogs);
+
+impl DataRecordBatch<LogRecord> for EngineLogRecordBatch {
+    fn drain<S: DataEngineState, F>(&mut self, state: &mut S, action: F) -> Result<(), Error>
+    where
+        F: Fn(&mut S, LogRecord) -> Result<(), Error>,
+    {
+        // Drain log_records by value, passing each to the closure
+        // let log_records = std::mem::take(&mut self.log_records); // Faster than Vec::drain for all
+        // for record in log_records {
+        //     action(state, record)?; // Early exit on error
+        // }
+        Ok(())
+    }
+}
+
+pub fn create_data_engine() -> Result<DataEngine, Error> {
+    let mut data_engine = DataEngine::new();
+
+    // data_engine.register::<TestResource>()?;
+    // data_engine.register::<TestInstrumentationScope>()?;
+    // data_engine.register::<TestLogRecord>()?;
+
+    Ok(data_engine)
+}
 
 // initialize the global state to an empty instance
 static GLOBAL_STATE: Lazy<Mutex<String>> = Lazy::new(|| {
@@ -31,16 +59,17 @@ pub extern "C" fn process(buf: *const u8, len: usize) -> i32 {
             scopeLogs.log_records.iter().for_each(|log_record| {
                 println!("Log Record: {:?}", log_record);
             });
+
             let mut pipeline = PipelineExpression::new();
 
-            pipeline.add_data_expression(DiscardDataExpression::new_with_predicate(
-                EqualToLogicalExpression::new(
-                    ResolveValueExpression::new("event_id"),
-                    StaticValueExpression::new(AnyValue::new_long_value(1)),
-                ),
-            ));
+            // pipeline.add_data_expression(DiscardDataExpression::new_with_predicate(
+            //     EqualToLogicalExpression::new(
+            //         ResolveValueExpression::new("event_id"),
+            //         StaticValueExpression::new(AnyValue::new_long_value(1)),
+            //     ),
+            // ));
 
-            let data_engine = common::create_data_engine();
+            let data_engine = create_data_engine();
             // let results = data_engine.process_complete_batch(&pipeline, &mut scopeLogs.log_records);
             0
         }
--- rust/experimental/query_engine/engine-api/test/go2rust/main.go
+++ rust/experimental/query_engine/engine-api/test/go2rust/main.go
@@ -1,22 +1,16 @@
 package main
 
 /*
-#cgo LDFLAGS: -L. -lengine_api
+#cgo LDFLAGS: -L. -lengine_api -lm
 #include "engine_api.h"
 */
 import "C"
 
-// import "go.opentelemetry.io/proto/otlp/collector/logs/v1"
-// import "go.opentelemetry.io/proto/otlp/logs/v1"
-import "go.opentelemetry.io/collector/pdata/plog"
-// import "go.opentelemetry.io/collector/pdata/internal"
-// import "example.com/go2rust/proto/otlp/collector/logs/v1"
 import logs_v1 "go.opentelemetry.io/proto/otlp/logs/v1"
 import common_v1 "go.opentelemetry.io/proto/otlp/common/v1"
 import "google.golang.org/protobuf/proto"
-import "reflect"
 
-func main() {
+func init_scope_logs() *logs_v1.ScopeLogs {
 	scope_logs := &logs_v1.ScopeLogs{
 		LogRecords: []*logs_v1.LogRecord{
 			{
@@ -45,11 +39,31 @@ func main() {
 							},
 						},
 					},
-					{
-						Key: "key3",
+				},
+			},
+			{
+				TimeUnixNano: 1234567891,
+				SeverityNumber: logs_v1.SeverityNumber_SEVERITY_NUMBER_ERROR,
+				SeverityText: "Error",
+				Body: &common_v1.AnyValue{
+					Value: &common_v1.AnyValue_StringValue{
+						StringValue: "This is a log error",
+					},
+				},
+				Attributes: []*common_v1.KeyValue{
+					{	
+						Key: "key1",
 						Value: &common_v1.AnyValue{
 							Value: &common_v1.AnyValue_StringValue{
-								StringValue: "value3",
+								StringValue: "value1",
+							},
+						},
+					},
+					{
+						Key: "key2",
+						Value: &common_v1.AnyValue{
+							Value: &common_v1.AnyValue_StringValue{
+								StringValue: "value2",
 							},
 						},
 					},
@@ -57,11 +71,13 @@ func main() {
 			},
 		},
 	}
-	println("Request type:", reflect.TypeOf(scope_logs).String())
-	data, _ := proto.Marshal(scope_logs) // Marshal the request to protobuf format
-	println("Data length:", len(data))
-	println("Data type", reflect.TypeOf(data).String())
+	return scope_logs
+}
 
-	C.init_query_engine(C.CString("table name | key1, key2, key3 | filter key1 = 'value1' and key2 = 'value2'"))
+func main() {
+	C.init_query_engine(C.CString("Log | body | filter SeverityNumber == 1")) // Initialize the query engine with a query
+
+	scope_logs := init_scope_logs() // Initialize the ScopeLogs structure
+	data, _ := proto.Marshal(scope_logs) // Marshal the request to protobuf format
 	C.process((*C.char)(C.CBytes(data)), C.size_t(len(data)))
 }
